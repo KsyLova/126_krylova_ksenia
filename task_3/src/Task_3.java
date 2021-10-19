3812953242c5164f24bf9a6e8682baf496007fb3
KsyLova <cenylova@gmail.com>
2021-10-19 11:55:46 +0300

    task3

--- task_3/src/Task_3.java
+++ task_3/src/Task_3.java
@@ -3,7 +3,23 @@ public class Task_3 implements Task_3_base {
     public int subtask_1_for(int n1, int n2, int a, int b) {
         // подсчитать, сколько чисел, кратных a, но не кратных b,
         // находится между числами n1 и n2 включительно
-        return 0;
+        int k;
+        k=0;
+        if (n1<n2) {
+            for (int i = n1; i <= n2; i++) {
+                if (i % a == 0 && i % b != 0) {
+                    k++;
+                }
+            }
+        }
+        else{
+            for (int i = n2; i <= n1; i++) {
+                if (i % a == 0 && i % b != 0) {
+                    k++;
+                }
+            }
+        }
+        return k;
     }
 
     @Override
@@ -17,7 +33,18 @@ public class Task_3 implements Task_3_base {
         // ...
         // Найти, какое число будет находиться в этой последовательности
         // на позиции num
-        return 0;
+        int a = num;
+        if ( a == 1)
+            return 1;
+        int k = 1;
+        for (int i = 2; i <=num; i++) {
+            for (int j = 1;j <=i;j++) {
+                k+=1;
+                if ( k==num)
+                    return i;
+            }
+        }
+        return num;
     }
 
     @Override
@@ -26,7 +53,13 @@ public class Task_3 implements Task_3_base {
         // a(0) = num
         // a(n) = a(n - 1) * d + 1
         // Найти сумму первых cnt элементов последовательности
-        return 0;
+        int sum = 0;
+        int a = num;
+        for ( int i=1; i<=cnt; i++){
+            sum=sum+a;
+            a=a*d+1;
+        }
+        return sum;
     }
 
     @Override
@@ -35,6 +68,14 @@ public class Task_3 implements Task_3_base {
         // S(n) = 1 + 1 * 2 + 1 * 2 * 3 + ... + n!
         // для заданного n
         // (n! - это n-факториал. Кто не знает - гуглите)
-        return 0;
+        int Sn=0;
+        for (int i = 1; i <= n; i++) {
+            int fact=1;
+            for (int j = 1; j <= i; j++) {
+                fact = fact * j;
+            }
+            Sn = Sn + fact;
+        }
+        return Sn;
     }
 }
